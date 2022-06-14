b72ec63551b849630d2bf28b8ffdc87339327bc7
MirkusX <104134633+MirkusX@users.noreply.github.com>
2022-06-14 13:41:49 +0200

    fixed bug
    fixed symbol and numbers to reappaering after unchecking "toggle symbol and number" checkbox.

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ function clearInput() {
 function generate() {
     if (toggleSymNum.checked === true) {
         characters = characters.splice(0, 51)
+    } else if (toggleSymNum.checked === false) {
+        characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+        "/"];
     }
     setLength = setLengthInput.value
     for (i = 0; i < setLength; i++) {
